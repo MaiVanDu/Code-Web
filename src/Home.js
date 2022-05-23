116b0011f181b89189383716ebe92343548ced60
Du <maivandu16012000@gmail.com>
2022-05-23 18:41:47 +0700

    lan 1

---
+++ src/Home.js
@@ -0,0 +1,109 @@
+import React from "react";
+
+function Home(){
+    return(
+        <section className="slider-product-one">
+          <div className="container">
+            <div className="slider-product-one-content">
+              <div className="slider-product-one-content-title">
+                <h2> Săn Sale mỗi ngày</h2>
+              </div>
+              <div className="slider-product-one-content-items">
+                <div className="slider-product-one-content-item">
+                  <img src="img/Logo-Laptop6.jpg" alt="" />
+                  <div className="slider-product-one-content-item-text">
+                    <li><img src="img/icon1.png" alt="" /><p>Trợ giá khuyến mãi</p></li>
+                    <li>Laptop Dell Gaming </li>
+                    <li>Online giá rẻ</li>
+                    <li><a href>25.990.000<sup>đ</sup></a></li>
+                    <li>18.500.000<sup>đ</sup></li>
+                    <li>Quà tặng 400.000<sup>đ</sup></li>
+                    <li>
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                    </li>
+                  </div>
+                </div>
+                <div className="slider-product-one-content-item">
+                  <img src="img/Logo-Laptop7.jpg" alt="" />
+                  <div className="slider-product-one-content-item-text">
+                    <li><img src="img/icon1.png" alt="" /><p>Trợ giá khuyến mãi</p></li>
+                    <li>Laptop Dell Gaming </li>
+                    <li>Online giá rẻ</li>
+                    <li><a href>25.990.000<sup>đ</sup></a></li>
+                    <li>18.500.000<sup>đ</sup></li>
+                    <li>Quà tặng 400.000<sup>đ</sup></li>
+                    <li>
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                    </li>
+                  </div>
+                </div>
+                <div className="slider-product-one-content-item">
+                  <img src="img/Logo-Laptop3.jpg" alt="" />
+                  <div className="slider-product-one-content-item-text">
+                    <li><img src="img/icon1.png" alt="" /><p>Trợ giá khuyến mãi</p></li>
+                    <li>Laptop Dell Gaming </li>
+                    <li>Online giá rẻ</li>
+                    <li><a href>25.990.000<sup>đ</sup></a></li>
+                    <li>18.500.000<sup>đ</sup></li>
+                    <li>Quà tặng 400.000<sup>đ</sup></li>
+                    <li>
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                    </li>
+                  </div>
+                </div>
+                <div className="slider-product-one-content-item">
+                  <img src="img/Logo-Laptop4.jpg" alt="" />
+                  <div className="slider-product-one-content-item-text">
+                    <li><img src="img/icon1.png" alt="" /><p>Trợ giá khuyến mãi</p></li>
+                    <li>Laptop Dell Gaming </li>
+                    <li>Online giá rẻ</li>
+                    <li><a href>25.990.000<sup>đ</sup></a></li>
+                    <li>18.500.000<sup>đ</sup></li>
+                    <li>Quà tặng 400.000<sup>đ</sup></li>
+                    <li>
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                    </li>
+                  </div>
+                </div>
+                <div className="slider-product-one-content-item">
+                  <img src="img/Logo-Laptop5.jpg" alt="" />
+                  <div className="slider-product-one-content-item-text">
+                    <li><img src="img/icon1.png" alt="" /><p>Trợ giá khuyến mãi</p></li>
+                    <li>Laptop Dell Gaming </li>
+                    <li>Online giá rẻ</li>
+                    <li><a href>25.990.000<sup>đ</sup></a></li>
+                    <li>18.500.000<sup>đ</sup></li>
+                    <li>Quà tặng 400.000<sup>đ</sup></li>
+                    <li>
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                      <i className="fa-solid fa-star" />
+                    </li>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+    )
+
+}
+export default Home;
